c78ef64acaab1c773248e07d3d9b946c1d776ef8
Marcel-MD <marcelvlasenco@gmail.com>
2023-04-21 10:37:59 +0300

    Add message ack by client

--- lib/consumer/client.ex
+++ lib/consumer/client.ex
@@ -9,16 +9,37 @@ defmodule Consumer.Client do
   def init(socket) do
     client_pid = self()
     Task.start_link(fn -> serve(socket, client_pid) end)
-    {:ok, socket}
+    {:ok, {socket, []}}
+  end
+
+  def ack(pid, hash) do
+    GenServer.cast(pid, {:ack, hash})
   end
 
   def notify(pid, topic, data) do
     GenServer.cast(pid, {:notify, topic, data})
   end
 
-  def handle_cast({:notify, topic, data}, socket) do
+  def handle_cast({:notify, topic, data}, {socket, messages}) do
+    messages = messages ++ [{topic, data}]
     write_data(topic, data, socket)
-    {:noreply, socket}
+    {:noreply, {socket, messages}}
+  end
+
+  def handle_cast({:ack, hash}, {socket, messages}) do
+    case messages do
+      [] ->
+        {:noreply, {socket, []}}
+      [{topic, data} | tail] ->
+        valid_hash = :crypto.hash(:md5, data) |> Base.encode16(case: :lower)
+        Logger.info("[#{__MODULE__}] Valid hash: #{valid_hash} - Received hash: #{hash}")
+        if valid_hash == hash do
+          {:noreply, {socket, tail}}
+        else
+          write_data(topic, data, socket)
+          {:noreply, {socket, messages}}
+        end
+    end
   end
 
   defp serve(socket, client_pid) do
@@ -35,6 +56,13 @@ defmodule Consumer.Client do
             topic = String.trim(topic)
             Logger.info("[#{__MODULE__}] Unsubscribing from topic #{topic}")
             Consumer.ClientManager.unsubscribe(client_pid, topic)
+          ["ACK", hash] ->
+            hash = String.trim(hash)
+            Logger.info("[#{__MODULE__}] Acknowledging message #{hash}")
+            Consumer.Client.ack(client_pid, hash)
+          _ ->
+            Logger.error("[#{__MODULE__}] Invalid response from client: #{line}")
+            write_line("ERROR\n", socket)
         end
       {:error, err} ->
         Logger.error("[#{__MODULE__}] : #{inspect err}")
